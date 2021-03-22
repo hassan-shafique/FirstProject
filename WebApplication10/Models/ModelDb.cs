e8dc1c545e8f9c33883f263f9dd04377d8f9b22d
hassan-shafique <hassanshafiq2011@gmail.com>
2021-03-22 17:25:04 +0500

    Add project files.

---
+++ WebApplication10/Models/ModelDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication10.Models
+{
+    public class ModelDb : DbContext
+    {
+        public ModelDb(DbContextOptions<ModelDb> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
